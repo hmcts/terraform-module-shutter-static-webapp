e7fdb2b89ff06f86e7d313b73929ffbaf152ac14
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-07-17 14:09:52 +0100

    initial

---
+++ inputs-required.tf
@@ -0,0 +1,37 @@
+variable "subscription_id" {
+  description = "Enter Azure subscription id."
+}
+variable "location" {
+  description = "Enter Azure location."
+  default     = "West Europe"
+
+}
+variable "env" {
+  description = "Enter the environment. eg prod, aat"
+}
+
+variable "product" {
+  description = "The name of the product."
+}
+variable "builtFrom" {
+  description = "The name of the Github repo."
+}
+
+variable "expiresAfter" {
+  description = "Expiration date"
+}
+
+variable "project" {
+  description = "The name of the project."
+}
+variable "tag" {
+  description = "Enter the tags."
+
+}
+variable "shutter_apps" {
+  default = []
+}
+variable "dns_zone_resource_group_name" {
+  description = "Resource group name for DNS zone"
+  default     = "dns_zone_resource_group_name"
+}
---
+++ static-webapp.tf
@@ -0,0 +1,25 @@
+
+resource "azurerm_static_site" "swebapp" {
+  for_each            = { for frontend in var.shutter_apps : frontend.name => frontend }
+  name                = each.value.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  tags                = var.tags
+}
+
+resource "azurerm_dns_cname_record" "cname_record" {
+  for_each            = { for frontend in var.shutter_apps : frontend.name => frontend }
+  provider            = azurerm.dnszone
+  name                = trimsuffix(trimsuffix(each.value.custom_domain, each.value.dns_zone_name), ".")
+  zone_name           = each.value.dns_zone_name
+  resource_group_name = var.dns_zone_resource_group_name
+  ttl                 = 300
+  record              = azurerm_static_site.swebapp[each.key].default_host_name
+}
+
+resource "azurerm_static_site_custom_domain" "custom_domain" {
+  for_each        = { for frontend in var.shutter_apps : frontend.name => frontend }
+  static_site_id  = azurerm_static_site.swebapp[each.key].id
+  domain_name     = each.value.custom_domain
+  validation_type = "cname-delegation"
+}
