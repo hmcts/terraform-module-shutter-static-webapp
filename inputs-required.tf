87a1ba5f4049b88efe7eddcf23012c9e6d4e5995
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-07-17 15:26:28 +0100

    update

--- inputs-required.tf
+++ inputs-required.tf
@@ -1,28 +1,12 @@
-variable "subscription_id" {
-  description = "Enter Azure subscription id."
+variable "resource_group_name" {
+  description = "Enter name of Azure resource group"
+  type        = string
+
 }
 variable "location" {
   description = "Enter Azure location."
   default     = "West Europe"
 
-}
-variable "env" {
-  description = "Enter the environment. eg prod, aat"
-}
-
-variable "product" {
-  description = "The name of the product."
-}
-variable "builtFrom" {
-  description = "The name of the Github repo."
-}
-
-variable "expiresAfter" {
-  description = "Expiration date"
-}
-
-variable "project" {
-  description = "The name of the project."
 }
 variable "tags" {
   description = "Enter the tags."
