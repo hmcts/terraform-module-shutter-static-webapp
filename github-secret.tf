da41f513a2f3a61041f6ef4d19de1542132a7aa5
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-07-18 14:26:21 +0100

    update

--- github-secret.tf
+++ github-secret.tf
@@ -2,12 +2,12 @@ data "github_repository" "repo" {
   full_name = "hmcts/azure-shutter-pages"
 }
 
-resource "github_actions_secret" "secret" {
+/* resource "github_actions_secret" "secret" {
   for_each        = { for frontend in var.shutter_apps : frontend.name => frontend }
   repository      = data.github_repository.repo.id
   secret_name     = each.value.name
   plaintext_value = azurerm_static_site.swebapp[each.key].api_key
-}
+} */
 
 output "github"{
     value = data.github_repository.repo.id
