d736a19adcfde9d6fc36b13a57f0410fc498f267
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-07-18 13:18:27 +0100

    update

--- github-secret.tf
+++ github-secret.tf
@@ -4,7 +4,7 @@ data "github_repository" "repo" {
 
 resource "github_actions_secret" "example_secret" {
   for_each        = { for frontend in var.shutter_apps : frontend.name => frontend }
-  repository      = data.github_repository.id
+  repository      = data.github_repository.repo.id
   secret_name     = each.value.name
   plaintext_value = azurerm_static_site.swebapp[each.key].api_key
 }
