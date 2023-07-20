71c7c9a80a66969ee8f8df77a822b3ad3df4549c
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-07-20 12:49:30 +0100

    update

--- static-webapp.tf
+++ static-webapp.tf
@@ -22,7 +22,7 @@ resource "azurerm_dns_txt_record" "zone_validate" {
   resource_group_name = var.dns_zone_resource_group_name
   ttl                 = 300
   record {
-    value = azurerm_static_site_custom_domain.custom_domain.validation_token
+    value = azurerm_static_site_custom_domain.custom_domain[each.key].validation_token
   }
 }
 
