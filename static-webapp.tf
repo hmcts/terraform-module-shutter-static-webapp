da767a2b1d2856bff9dd308a3a4574b1f7de6937
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-07-31 12:08:07 +0100

    update

--- static-webapp.tf
+++ static-webapp.tf
@@ -11,10 +11,10 @@ resource "azurerm_static_site_custom_domain" "custom_domain" {
   for_each        = { for frontend in var.shutter_apps : frontend.name => frontend }
   static_site_id  = azurerm_static_site.swebapp[each.key].id
   domain_name     = each.value.custom_domain
-  validation_type = "dns-txt-token"
+  validation_type = "cname-delegation"
 }
 
-resource "azurerm_dns_txt_record" "zone_validate" {
+/* resource "azurerm_dns_txt_record" "zone_validate" {
   for_each            = { for frontend in var.shutter_apps : frontend.name => frontend }
   provider            = azurerm.dnszone
   name                = each.value.custom_domain == each.value.dns_zone_name ? "@" : join(".", ["_dnsauth", trimsuffix(trimsuffix(each.value.custom_domain, each.value.dns_zone_name), ".")])
@@ -25,12 +25,12 @@ resource "azurerm_dns_txt_record" "zone_validate" {
     # ISSUE: https://github.com/hashicorp/terraform-provider-azurerm/issues/14750
     value = azurerm_static_site_custom_domain.custom_domain[each.key].validation_token == "" ? "validated" : azurerm_static_site_custom_domain.custom_domain[each.key].validation_token
   }
-}
+} */
 
 resource "azurerm_dns_cname_record" "cname_record" {
-  for_each            = { for frontend in var.shutter_apps : frontend.name => frontend if frontend.custom_domain != frontend.dns_zone_name }
+  for_each            = { for frontend in var.shutter_apps : frontend.name => frontend }
   provider            = azurerm.dnszone
-  name                = join("-", [trimsuffix(trimsuffix(each.value.custom_domain, each.value.dns_zone_name), "."), "shutter"])
+  name                = frontend.custom_domain != frontend.dns_zone_name ? join("-", [trimsuffix(trimsuffix(each.value.custom_domain, each.value.dns_zone_name), "."), "shutter"]) : join("-", [azurerm_static_site.swebapp[each.key].name, shutter]) 
   zone_name           = each.value.dns_zone_name
   resource_group_name = var.dns_zone_resource_group_name
   ttl                 = 300
