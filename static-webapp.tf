70bfa24bec2f1107093e77195179824542528f2e
mokainos <mohana.yalamanchi@justice.gov.uk>
2023-07-31 14:04:10 +0100

    update

--- static-webapp.tf
+++ static-webapp.tf
@@ -11,13 +11,13 @@ resource "azurerm_static_site_custom_domain" "custom_domain" {
   for_each        = { for frontend in var.shutter_apps : frontend.name => frontend }
   static_site_id  = azurerm_static_site.swebapp[each.key].id
   domain_name     = each.value.custom_domain
-  validation_type = "cname-delegation"
+  validation_type = each.value.custom_domain == each.value.dns_zone_name ? "cname-delegation" : "dns-txt-token"
 }
 
-/* resource "azurerm_dns_txt_record" "zone_validate" {
-  for_each            = { for frontend in var.shutter_apps : frontend.name => frontend }
+resource "azurerm_dns_txt_record" "zone_validate" {
+  for_each            = { for frontend in var.shutter_apps : frontend.name => frontend if fronend.custom_domain != frontend.dns_zone_name }
   provider            = azurerm.dnszone
-  name                = each.value.custom_domain == each.value.dns_zone_name ? "@" : join(".", ["_dnsauth", trimsuffix(trimsuffix(each.value.custom_domain, each.value.dns_zone_name), ".")])
+  name                = join(".", ["_dnsauth", trimsuffix(trimsuffix(each.value.custom_domain, each.value.dns_zone_name), ".")])
   zone_name           = each.value.dns_zone_name
   resource_group_name = var.dns_zone_resource_group_name
   ttl                 = 300
@@ -25,7 +25,7 @@ resource "azurerm_static_site_custom_domain" "custom_domain" {
     # ISSUE: https://github.com/hashicorp/terraform-provider-azurerm/issues/14750
     value = azurerm_static_site_custom_domain.custom_domain[each.key].validation_token == "" ? "validated" : azurerm_static_site_custom_domain.custom_domain[each.key].validation_token
   }
-} */
+}
 
 resource "azurerm_dns_cname_record" "cname_record" {
   for_each            = { for frontend in var.shutter_apps : frontend.name => frontend }
