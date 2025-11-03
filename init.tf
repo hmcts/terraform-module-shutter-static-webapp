49fbb6fe2230ab185d7e20f916d6cf169cf8ed58
Asaleem-hmcts <Aamir.Saleem@HMCTS.NET>
2025-11-03 15:11:44 +0000

    update 4.51.0

--- init.tf
+++ init.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source                = "hashicorp/azurerm"
-      version               = "4.47.0"
+      version               = "4.51.0"
       configuration_aliases = [azurerm.dnszone]
     }
     github = {
